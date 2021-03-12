fdb4c196a75bb1549de5c76ecad1c8b30e8facdf
Adam Mayer <r.adammayer@gmail.com>
2021-03-11 21:44:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -104,22 +104,22 @@ app.get('./lotto', (req, res) =>{
             return res
               .status(400)
               .send("numbers must contain 6 integers between 1 and 20");
-          }      
+        }      
           // fully validated numbers
           // here are the 20 numbers to choose from
           const stockNumbers = Array(20).fill(1).map((_, i) => i + 1);
           //randomly choose 6
           const winningNumbers = [];
-          for(let i = 0; i < 6; i++) {
+        for(let i = 0; i < 6; i++) {
             const ran = Math.floor(Math.random() * stockNumbers.length);
             winningNumbers.push(stockNumbers[ran]);
             stockNumbers.splice(ran, 1);
-          }
+        }
           //compare the guesses to the winning number
           let diff = winningNumbers.filter(n => !guesses.includes(n));
           // construct a response
           let responseText;
-          switch(diff.length){
+        switch(diff.length){
             case 0: 
               responseText = 'Wow! Unbelievable! You could have won the mega millions!';
               break;
@@ -131,7 +131,7 @@ app.get('./lotto', (req, res) =>{
               break;
             default:
               responseText = 'Sorry, you lose';  
-          }
+        }
           // uncomment below to see how the results ran
           // res.json({
           //   guesses,
@@ -140,7 +140,7 @@ app.get('./lotto', (req, res) =>{
           //   responseText
           // });
           res.send(responseText);
-        });
+       
 
 });
 
